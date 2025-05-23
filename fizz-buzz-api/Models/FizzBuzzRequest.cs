343f5909c08074ae51ef07db06692a63fb02d2a7
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2025-05-23 17:59:40 -0500

    initial fizz buzz api project

---
+++ fizz-buzz-api/Models/FizzBuzzRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz.Models;
+
+public class FizzBuzzItem
+{
+    public int Multiple { get; init; }
+    public string? WordToPrint { get; init; }
+}
+
+public class FizzBuzzRequest
+{
+    public FizzBuzzItem[] Multiples { get; init; } = [];
+    public int MaxNumber { get; init; } = 100;
+}
