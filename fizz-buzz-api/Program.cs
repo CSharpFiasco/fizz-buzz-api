20bac6b5c95aa32d0b634d159283a12592e38b56
Carlos Martos <40156600+CSharpFiasco@users.noreply.github.com>
2025-05-25 13:48:03 -0500

    Add logging

--- fizz-buzz-api/Program.cs
+++ fizz-buzz-api/Program.cs
@@ -1,12 +1,30 @@
 using FizzBuzz.CachePolicies;
 using FizzBuzz.Models;
 using FizzBuzz.Services;
-using Microsoft.Extensions.Options;
+using Serilog;
+using Serilog.Events;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IFizzBuzzService, FizzBuzzService>();
+
+builder.Services.AddSerilog(config =>
+{
+    config
+        .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+        .WriteTo.File(
+           Path.Combine(Environment.CurrentDirectory, "LogFiles", "Application", "diagnostics.txt"),
+           rollingInterval: RollingInterval.Day,
+           fileSizeLimitBytes: 10 * 1024 * 1024,
+           retainedFileCountLimit: 2,
+           rollOnFileSizeLimit: true,
+           shared: true,
+           flushToDiskInterval: TimeSpan.FromSeconds(1))
+        ;
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -36,6 +54,8 @@ var app = builder.Build();
 
 app.UseCors(MyAllowSpecificOrigins);
 
+app.UseSerilogRequestLogging();
+
 app.UseExceptionHandler();
 app.UseStatusCodePages();
 
@@ -49,7 +69,8 @@ if (app.Environment.IsDevelopment())
 // todo: setup probe for liveness and readiness
 app.MapHealthChecks("/");
 
-app.MapPost("/", (FizzBuzzRequest request, IFizzBuzzService service) => {
+app.MapPost("/", (FizzBuzzRequest request, IFizzBuzzService service) =>
+{
     return service.Process(request);
 }).CacheOutput("CachePost");
 
